f6fe7f5526e4b09549f50dcfc4ae40509a7f4b8f
root <root@ourfamilynetwork.org-wordpress-s-ubuntu-18.04>
2019-11-02 17:36:01 +0000

    added a proper nav bar, styling to fonts, featured content area on the homepage

--- template-home.php
+++ template-home.php
@@ -4,50 +4,62 @@
 	Template Name: Home Page
 */
 
-
 if( is_user_logged_in() ){
 	get_header(); ?>
 
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<!-- https://codepen.io/jeffglenn/pen/KNYoKa/ -->
-		<section id="timeline">
+			<div class="featured-content">
+				<img class="featured-content-img"
+					src="<?php echo get_field('featured_content_img') ?>"
+				/>
+				<div class="featured-content-copy">
+					<p><?php echo get_field("featured_content_copy"); ?></p>
+				</div>
+			</div>
 
-			<?php
-				$args = array(
-					'posts_per_page' => 4,
-					'start_date' => 'now',
-				);
-				$events = tribe_get_events($args);
-				foreach( $events as $event ){ ?>
-					<div class="tl-item">
-						<div class="tl-bg" style="background-image: url(<?php echo get_the_post_thumbnail_url($event->ID); ?>)"></div>
-						<a href="<?php echo $event->guid; ?>">
-							<div class="tl-year">
-								<h2><?php echo $event->post_title; ?></h2>
-								<p class="f2 heading--sanSerif"><?php echo date( "M  d  Y", strtotime($event->event_date) ); ?></p>
-							</div>
-							<div class="tl-content">
-								<p><?php echo $event->post_excerpt; ?></p>
-							</div>
-						</a>
-					</div>
-				<?php } ?>
+			<!-- https://codepen.io/jeffglenn/pen/KNYoKa/ -->
+			<section id="timeline">
+				<?php
+					$args = array(
+						'posts_per_page' => 4,
+						'start_date' => 'now',
+					);
+					$events = tribe_get_events($args);
+					foreach( $events as $event ){ ?>
+						<div class="tl-item">
+							<div class="tl-bg" style="background-image: url(<?php echo get_the_post_thumbnail_url($event->ID); ?>)"></div>
+							<a href="<?php echo $event->guid; ?>">
+								<div class="tl-year">
+									<h2><?php echo $event->post_title; ?></h2>
+									<p class="f2 heading--sanSerif"><?php echo date( "M  d  Y", strtotime($event->event_date) ); ?></p>
+								</div>
+								<div class="tl-content">
+									<p><?php echo $event->post_excerpt; ?></p>
+								</div>
+							</a>
+						</div>
+					<?php } ?>
 
-		</section>
-		<?php
-			if (have_posts()){
-				while (have_posts()){
+			</section>
 
-					the_post();
-					the_content();
-
-				}
-			}
-		?>
+			<section id="user-content">
+				<?php
+					if (have_posts()){
+						while (have_posts()){
+							the_post();
+							the_content();
+						}
+					}
+				?>
+			</section>
 
 <?php }else{ ?>
+
+	<!--
+		If user not logged in, display the Under Construction page.
+	-->
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main">
 		<style>
@@ -71,7 +83,8 @@ if( is_user_logged_in() ){
 			<h1>Currently Under Construction</h1>
 			<h4>Please Log in to view website</h4>
 		</div>
-<?php } ?>
+
+<?php } edit_post_link(); ?>
 
 	</main><!-- #main -->
 </section><!-- #primary -->
