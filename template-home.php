cb4719b45aac7a83145c2c357a1e623505cb9afc
root <kbcarte@gmail.com>
2019-11-14 00:21:57 +0000

    made it responsive/mobile-ish

--- template-home.php
+++ template-home.php
@@ -20,7 +20,7 @@ if( is_user_logged_in() ){
 			</div>
 
 			<!-- https://codepen.io/jeffglenn/pen/KNYoKa/ -->
-			<section id="timeline">
+			<section id="timeline" class="desktop">
 				<?php
 					$args = array(
 						'posts_per_page' => 4,
@@ -42,7 +42,28 @@ if( is_user_logged_in() ){
 							</a>
 						</div>
 					<?php } ?>
+			</section>
 
+
+			<section id="mobile-timeline" class="mobile">
+				<?php
+					$args = array(
+						'posts_per_page' => 4,
+						'start_date' => 'now',
+					);
+					$events = tribe_get_events($args);
+					foreach( $events as $event ){ ?>
+						<a href="<?php echo $event->guid; ?>">
+							<div class="mobile-item" style="background-image: url(<?php echo get_the_post_thumbnail_url($event->ID); ?>)">
+								<div class="mobile-year">
+									<div class="dark-overlay">
+										<h2 class="mobile-event-title"><?php echo $event->post_title; ?></h2>
+										<p class="f2 heading--sanSerif"><?php echo date( "M  d  Y", strtotime($event->event_date) ); ?></p>
+									</div>
+								</div>
+							</div>
+						</a>
+					<?php } ?>
 			</section>
 
 			<section id="user-content">
